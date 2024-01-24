b2e01caae0a2e1db4faa6a9de278f3897228e127
MatreRajesh <matrerajesh1234@gmail.com>
2024-01-24 16:24:30 +0530

    slider-creation

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,16 +5,19 @@ const Home = () => {
   return (
     <>
     <div className='Home'>
+      <div className="Slider">
+
+      
             <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
           <div className="carousel-inner">
             <div className="carousel-item active">
-              <img src="/Images/Slider/Slider1.jpg" className="d-block w-100" alt="..."/>
+              <img src="https://media.gettyimages.com/id/1233011280/photo/francisco-martin-demonstrates-the-use-of-a-pulse-oximeter-that-measures-blood-oxygen-levels.jpg?s=612x612&w=0&k=20&c=uM26rK_gdONe0BaQ5DHcY8cviy1x1KON0zhv-colKGg=" className="d-block w-100" alt="..."/>
+            </div>
+            <div className="carousel-item ">
+              <img src="https://media.gettyimages.com/id/1363371727/photo/man-testing-his-oxygen-saturation-at-home-using-pulse-oximeter.jpg?s=612x612&w=0&k=20&c=2ADptN4SktO_VV2ak6ZN1TsT_o2mPZ82xLtXMVQ1apU="  className='d-block w-100' alt="..."/>
             </div>
             <div className="carousel-item">
-              <img src="/Images/Slider/Slider2.jpg" alt="..."/>
-            </div>
-            <div className="carousel-item">
-              <img src="/Images/Slider/Slider3.jpg" alt="..."/>
+              <img src="https://media.gettyimages.com/id/1291425639/photo/oximeter-for-checking-oxygen-saturation-in-blood-on-male-hand.jpg?s=612x612&w=0&k=20&c=VtRcijdFbl5LRC2c-UlWHiEYwhin2jzP4ZsuTJYcPmA=" className='d-block w-100' alt="..."/>
             </div>
           </div>
           <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
@@ -25,6 +28,7 @@ const Home = () => {
             <span className="carousel-control-next-icon" aria-hidden="true"></span>
             <span className="visually-hidden">Next</span>
           </button>
+        </div>
         </div>
     </div>
     </>
